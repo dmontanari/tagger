87793d9bf3e1b6d34ae5bb2cd1e295a708feeaa0
Montanari <dmontanari@gmail.com>
2026-03-30 00:04:09 -0300

    createAuthMethod

--- gitutil/pushtag.go
+++ gitutil/pushtag.go
@@ -2,67 +2,14 @@ package gitutil
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/go-git/go-git/v6"
 	"github.com/go-git/go-git/v6/config"
-	"github.com/go-git/go-git/v6/plumbing/transport"
-	"github.com/go-git/go-git/v6/plumbing/transport/ssh"
-	"github.com/kevinburke/ssh_config"
-	cryptossh "golang.org/x/crypto/ssh"
 )
-
-func createAuthMethod(endpoint transport.Endpoint) (transport.AuthMethod, error) {
-
-	host := endpoint.Host
-
-	// Get IdentityFile from .ssh/config
-	// Expand "~" to full path
-	keyPath := ssh_config.Get(host, "IdentityFile")
-	if keyPath == "" {
-		return nil, fmt.Errorf("publik key not set")
-	}
-	if strings.HasPrefix(keyPath, "~/") {
-		home, _ := os.UserHomeDir()
-		keyPath = filepath.Join(home, keyPath[2:])
-	}
-
-	// Get username from .ssh/config
-	user := ssh_config.Get(host, "User")
-	if user == "" {
-		// Default fallback: user git
-		user = "git"
-	}
-
-	// Load key
-	if _, err := os.Stat(keyPath); err == nil {
-		auth, err := ssh.NewPublicKeysFromFile(user, keyPath, "")
-		if err == nil {
-			auth.HostKeyCallback = cryptossh.InsecureIgnoreHostKey()
-			return auth, nil
-		}
-	}
-
-	// Fallback to ssh-agent
-	auth, err := ssh.NewSSHAgentAuth(user)
-	if err == nil {
-		auth.HostKeyCallback = cryptossh.InsecureIgnoreHostKey()
-		return auth, nil
-	}
-
-	return nil, fmt.Errorf("error creating auth method")
-
-}
 
 func (g GitTags) Push(tagName string) error {
 
-	url := g.GetRemote()
-
-	endpoint, _ := transport.NewEndpoint(url)
-
-	authMethod, err := createAuthMethod(*endpoint)
+	authMethod, err := g.createAuthMethod()
 	if err != nil {
 		return err
 	}
--- gitutil/remote.go
+++ gitutil/remote.go
@@ -1,5 +1,17 @@
 package gitutil
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-git/go-git/v6/plumbing/transport"
+	"github.com/go-git/go-git/v6/plumbing/transport/ssh"
+	"github.com/kevinburke/ssh_config"
+	cryptossh "golang.org/x/crypto/ssh"
+)
+
 // Check if the repo have a remote
 func (g GitTags) HaveRemote() bool {
 
@@ -25,7 +37,7 @@ func (g GitTags) GetRemote() string {
 
 	cfg, _ := g.repository.Config()
 
-	rawRemote := cfg.Raw.Section("remote").Subsection("origin")
+	rawRemote := cfg.Raw.Section("remote").Subsection(g.remoteName)
 
 	// Note
 	//
@@ -45,5 +57,52 @@ func (g GitTags) GetRemote() string {
 	pushURL := rawRemote.Option("url")
 
 	return pushURL
+
+}
+
+// Create AuthMethod to connect to remote
+func (g GitTags) createAuthMethod() (transport.AuthMethod, error) {
+
+	url := g.GetRemote()
+
+	endpoint, _ := transport.NewEndpoint(url)
+
+	host := endpoint.Host
+
+	// Get IdentityFile from .ssh/config
+	// Expand "~" to full path
+	keyPath := ssh_config.Get(host, "IdentityFile")
+	if keyPath == "" {
+		return nil, fmt.Errorf("publik key not set")
+	}
+	if strings.HasPrefix(keyPath, "~/") {
+		home, _ := os.UserHomeDir()
+		keyPath = filepath.Join(home, keyPath[2:])
+	}
+
+	// Get username from .ssh/config
+	user := ssh_config.Get(host, "User")
+	if user == "" {
+		// Default fallback: user git
+		user = "git"
+	}
+
+	// Load key
+	if _, err := os.Stat(keyPath); err == nil {
+		auth, err := ssh.NewPublicKeysFromFile(user, keyPath, "")
+		if err == nil {
+			auth.HostKeyCallback = cryptossh.InsecureIgnoreHostKey()
+			return auth, nil
+		}
+	}
+
+	// Fallback to ssh-agent
+	auth, err := ssh.NewSSHAgentAuth(user)
+	if err == nil {
+		auth.HostKeyCallback = cryptossh.InsecureIgnoreHostKey()
+		return auth, nil
+	}
+
+	return nil, fmt.Errorf("error creating auth method")
 
 }
